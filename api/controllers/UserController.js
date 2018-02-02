3f39078981c76f0130f1d67c9d2ba0b592eafc54
Ahmed Radwan <ahmedrad@gmail.com>
2018-02-01 23:41:49 -0800

    first commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,53 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+  create1: async function(req, res) {
+    console.log(req);
+    try {
+      let result = await User.create(req.allParams());
+      res.json(result);
+    }
+    catch(err) {
+      res.negotiate(err);
+    }
+
+  },
+
+  create2: async function(req, res) {
+    console.log(req);
+    try {
+      let result = await User.findOrCreate({email: req.param('email')}, req.allParams());
+      res.json(result);
+    }
+    catch(err) {
+      res.negotiate(err);
+    }
+
+  },
+
+  show: async function(req, res) {
+    try {
+      let result = await User.find().decrypt();
+      res.json(result);
+    }
+    catch(err) {
+      res.negotiate(err);
+    }
+  },
+
+  showSignup1: function(req, res) {
+    res.view('user/signup1');
+  },
+
+  showSignup2: function(req, res) {
+    res.view('user/signup2');
+  },
+
+
+};
---
+++ config/routes.js
@@ -0,0 +1,42 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+
+
+  /***************************************************************************
+  *                                                                          *
+  * Make the view located at `views/homepage.ejs` your home page.            *
+  *                                                                          *
+  * (Alternatively, remove this and add an `index.html` file in your         *
+  * `assets` directory)                                                      *
+  *                                                                          *
+  ***************************************************************************/
+
+  '/': {
+    view: 'homepage'
+  },
+
+  'get /user/show': 'UserController.show',
+  'get /user/signup1': 'UserController.showSignup1',
+  'get /user/signup2': 'UserController.showSignup2',
+  'post /user/signup1': 'UserController.create1',
+  'post /user/signup2': 'UserController.create2'
+  /***************************************************************************
+  *                                                                          *
+  * Custom routes here...                                                    *
+  *                                                                          *
+  * If a request to a URL doesn't match any of the custom routes above, it   *
+  * is matched against Sails route blueprints. See `config/blueprints.js`    *
+  * for configuration options and examples.                                  *
+  *                                                                          *
+  ***************************************************************************/
+
+};
